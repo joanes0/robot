e1d1079b0a08e79f6a0956498e55cf355be914ab
unknown <joanesodanga464@gmail.com>
2021-03-24 18:18:15 +0300

    all folders component

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Degea David',
+        username: 'Degea',
+        email: 'david@manchester.com'
+    },
+    {
+        id: 2,
+        name: 'Victor Lindelof',
+        username: 'lindelof',
+        email: 'lindelof@manchester.com'
+    },
+    {
+        id: 3,
+        name: 'Erick Bailly',
+        username: 'Bailly',
+        email: 'erick@manchester.com'
+    },
+    {
+        id: 4,
+        name: 'Wangi Bisaka',
+        username: 'Wan Gi',
+        email: 'Bissaka@manchester.com'
+    },
+    {
+        id: 5,
+        name: 'Harry Maguire',
+        username: 'Maguire',
+        email: 'maguire@manchester.com'
+    },
+    {
+        id: 6,
+        name: 'Paul Pogba',
+        username: 'Pogba',
+        email: 'pogba@manchester.com'
+    },
+    {
+        id: 7,
+        name: 'Eddison Cavani',
+        username: 'Cavani',
+        email: 'cavani@manchester.com'
+    },
+    {
+        id: 8,
+        name: 'Juan Mata',
+        username: 'Mata',
+        email: 'mata@manchester.com'
+    },
+    {
+        id: 9,
+        name: 'Antony Martial',
+        username: 'Martial',
+        email: 'martial@manchester.com'
+    },
+    {
+        id: 10,
+        name: 'arcus ashford',
+        username: 'ashford',
+        email: 'marcus@manchester.com'
+    },
+    {
+        id: 11,
+        name: 'Masson Greenwood',
+        username: 'Greenwood',
+        email: 'mason@manchester.com'
+    },
+    {
+        id: 12,
+        name: 'Bruno Fernandez',
+        username: 'Bruno',
+        email: 'bruno@manchester.com'
+    },
+]
